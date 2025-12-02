ee6c1d344bd3c85bb0ec627ba3cc4228e61b6b1f
HazemShaban290 <shabanhazemapple@gmail.com>
2025-12-02 19:09:20 +0300

    Time: 6 ms (75.25%), Space: 12.6 MB (13.16%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        romanMap={
+            'I':1,
+            'V':5,
+            'X':10,
+            'L':50,
+            'C':100,
+            'D':500,
+            'M':1000,
+            'IV':4,
+            'IX':9,
+            'XL':40,
+            'XC':90,
+            'CD':400,
+            'CM':900
+        }
+        i=0
+        num=0   
+        while i<len(s):
+            if i+2<=len(s) and s[i:i+2] in romanMap:
+                num+=romanMap[s[i:i+2]] 
+                i+=2
+            else:
+                num+=romanMap[s[i]] 
+                i+=1
+        return num
