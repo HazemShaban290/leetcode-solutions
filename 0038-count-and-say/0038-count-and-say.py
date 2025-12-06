f2c24199ad4fcea1b9d7bfa20dd2615591700b79
HazemShaban290 <shabanhazemapple@gmail.com>
2025-12-06 17:38:01 +0300

    Time: 7 ms (92.64%), Space: 12.8 MB (2.61%) - LeetHub

---
+++ 0038-count-and-say/0038-count-and-say.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def countAndSay(self, n):
+        """
+        :type n: int
+        :rtype: str
+        """
+        def compress(string):
+            print(string)
+            compressed=[]
+            count=0
+            char=string[0]
+
+            for i in range(len(string)):
+                if char == string[i]:
+                    count+=1
+                else:
+                    compressed.append(str(count)+char)
+                    count=1
+                    char=string[i]
+            compressed.append(str(count)+char)
+            return ''.join(compressed)
+        def countAndSay(n):
+            if n==1:return '1'
+            return compress(countAndSay(n-1))
+        return countAndSay(n)
