1c75c59c22076ed7edecf2f2a3f4b840335a81f7
HazemShaban290 <shabanhazemapple@gmail.com>
2025-12-08 13:40:26 +0300

    Time: 4 ms (60.25%), Space: 12.4 MB (85.89%) - LeetHub

--- 0093-restore-ip-addresses/0093-restore-ip-addresses.py
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.py
@@ -8,23 +8,17 @@ class Solution(object):
         ip=[]
         def getIPAddresses(s,dots,i):
             if len(s)-i<(dots+1) or len(s)-i>(dots+1)*3:
-                
                 return 
             if dots ==-1 :
                 IPs.append('.'.join(ip[:]))
                 return
-            if int(s[i:i+1]) <=255:
-                ip.append(s[i:i+1])
-                getIPAddresses(s,dots-1,i+1)
-                ip.pop()
-            if i+2 <=len(s) and int(s[i:i+2]) <=255 and s[i]!='0':
-                ip.append(s[i:i+2])
-                getIPAddresses(s,dots-1,i+2)
-                ip.pop()
-            if i+3 <=len(s) and int(s[i:i+3]) <=255 and s[i]!='0':
-                ip.append(s[i:i+3])
-                getIPAddresses(s,dots-1,i+3)
-                ip.pop()
+            for charCount in range(1,4):
+                if i+charCount <=len(s) and int(s[i:i+charCount]) <=255:
+                    if charCount>1 and s[i]=='0': continue
+                    ip.append(s[i:i+charCount])
+                    getIPAddresses(s,dots-1,i+charCount)
+                    ip.pop()
+
         getIPAddresses(s,3,0)
         return IPs
 
