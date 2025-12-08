c52509ffda210365637db03d438e1e80ea5505ca
HazemShaban290 <shabanhazemapple@gmail.com>
2025-12-08 12:17:21 +0300

    Time: 0 ms (100%), Space: 12.7 MB (10.21%) - LeetHub

---
+++ 0091-decode-ways/0091-decode-ways.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def numDecodings(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        if len(s)==0 or s[0]=='0':return 0
+        visited={}
+        def numberOfWays(s,i):
+        
+            if i ==len(s):
+                return 1
+            if s[i]=='0':return 0
+            if i not in visited:
+                visited[i]=(numberOfWays(s,i+1) if int(s[i:i+1])>0 and  int(s[i:i+1])<27 else 0) + (numberOfWays(s,i+2) if i+2<=len(s) and int(s[i:i+2])>0 and  int(s[i:i+2])<27 else 0)
+            return visited[i]
+        return numberOfWays(s,0)
