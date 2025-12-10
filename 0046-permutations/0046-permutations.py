ac80ea9672944b1e2091b587bc7fab32f7ab90b8
HazemShaban290 <shabanhazemapple@gmail.com>
2025-12-10 21:19:23 +0300

    Time: 0 ms (100%), Space: 12.7 MB (18.55%) - LeetHub

---
+++ 0046-permutations/0046-permutations.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        permutation=[]
+        results=[]
+        def getAllPermutation():
+            
+            if len(nums) ==0:
+                results.append(permutation[:])
+                return 
+            for i in range(len(nums)):
+                permutation.append(nums[i])
+                x=nums.pop(i)
+                getAllPermutation()
+                
+                nums.insert(i,x)
+                permutation.pop()
+        getAllPermutation()
+        return results
