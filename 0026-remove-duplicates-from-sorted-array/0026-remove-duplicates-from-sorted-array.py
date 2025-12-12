19f48a4a40d2967590cfea6ce73efdbf0e2e4afa
HazemShaban290 <shabanhazemapple@gmail.com>
2025-12-12 22:06:31 +0300

    Time: 3 ms (61.64%), Space: 14 MB (38.49%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -4,9 +4,9 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        uniqueNumbers=1
-        currNum=nums[0]
-        for i in range(1,len(nums)):
+        uniqueNumbers=0
+        currNum=nums[0]-1
+        for i in range(0,len(nums)):
             if nums[i]!=currNum:
                 nums[uniqueNumbers]=nums[i]
                 currNum=nums[i]
