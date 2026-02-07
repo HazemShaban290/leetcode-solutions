9aafb82c6fe2a9dbb1af1c406a9e43caad83e19b
HazemShaban290 <shabanhazemapple@gmail.com>
2026-02-08 00:18:00 +0300

    Time: 35 ms (49.32%), Space: 22 MB (38.58%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -9,3 +9,9 @@ class Solution:
                 appearance+=hashMap[currSum - k]
             hashMap[currSum]=hashMap.get(currSum,0)+1
         return appearance
+
+    
+# prefix Sum for index i is sum from 0 -> i
+# if j>i and (prefix[j] - prefix[i])==k that means the sum of numbers from i+1 to j is equal to k
+# prefix[i] == prefix[j]-k   then the sum of numbers from i+1 to j is equal to k
+# if the prefix[j]-k  appears before that means the sum of numbers from i+1 to j is equal to k
