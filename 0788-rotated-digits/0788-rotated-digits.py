317fd1e23b48cb2e36b52d417eaa64011ec8846e
HazemShaban290 <shabanhazemapple@gmail.com>
2026-02-21 00:37:21 +0300

    Time: 23 ms (61.97%), Space: 19.5 MB (28.48%) - LeetHub

--- 0788-rotated-digits/0788-rotated-digits.py
+++ 0788-rotated-digits/0788-rotated-digits.py
@@ -1,15 +1,24 @@
 class Solution:
     def rotatedDigits(self, n: int) -> int:
-        good = {'2','5','6','9'}
-        bad = {'3','4','7'}
-        
-        count = 0
-        
-        for num in range(1, n+1):
-            s = str(num)
-            if any(c in bad for c in s):
-                continue
-            if any(c in good for c in s):
-                count += 1
+        goodNumbers=set()
+        skip={1,0,8}
+        rotated={2,5,6,9}
+        for i in range(1,n+1):
+            changed=False
+            x=i
+            while x !=0:
+                if x in goodNumbers:
+                    changed=True
+                    break
+                digit =x%10
+                x=x//10
+                if digit in rotated:
+                    changed=True
+                elif  digit not in skip:
+                    changed=False
+                    break
+            if changed :
+                goodNumbers.add(i)
+        return len(goodNumbers)
+
                 
-        return count
