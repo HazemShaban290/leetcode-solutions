9758ba61ddee383166bb26b3aa077515bdafc616
HazemShaban290 <shabanhazemapple@gmail.com>
2026-03-19 01:38:00 +0300

    Time: 225 ms (73.39%), Space: 21.1 MB (73.54%) - LeetHub

--- 0792-number-of-matching-subsequences/0792-number-of-matching-subsequences.py
+++ 0792-number-of-matching-subsequences/0792-number-of-matching-subsequences.py
@@ -1,20 +1,19 @@
+from collections import defaultdict
 class Solution:
-    def checkIsSubSeq(self,word1,word2):
-        p1 ,p2=0,0
-        while p1<len(word1) and p2<len(word2):
-            if word1[p1]==word2[p2]:
-                p1+=1
-                p2+=1
-            else:
-                p1+=1
-        return p2==len(word2)
     def numMatchingSubseq(self, s: str, words: List[str]) -> int:
-        counter={}
+        waiting =defaultdict(list)
         for word in words:
-            counter[word]=counter.get(word,0)+1
-        
+           waiting[word[0]].append(word)
         count=0
-        for word in counter.keys():
-            if self.checkIsSubSeq(s,word):
-                count+=counter[word]
+        for ch in s:
+            if ch in waiting:
+                waitingList=waiting[ch]
+                waiting[ch]=[]
+                while waitingList:
+                    word=waitingList.pop()
+                    if len(word)==1:
+                        count+=1
+                    else:
+                        waiting[word[1]].append(word[1:])
         return count
+
